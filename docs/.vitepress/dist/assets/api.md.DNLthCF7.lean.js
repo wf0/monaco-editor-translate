99de0859c2d12b70e78023d36e93938e97767bdb
weifegn <1982392655@qq.com>
2024-06-22 10:00:40 +0800

    fix:delete dist

--- docs/.vitepress/dist/assets/api.md.DNLthCF7.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as i,o as a,a3 as n,a4 as t}from"./chunks/framework.CrxK02rG.js";const y=JSON.parse('{"title":"Monaco Editor API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md"}'),l={name:"api.md"},p=n("",16),h=[p];function e(k,r,o,d,E,c){return a(),i("div",null,h)}const F=s(l,[["render",e]]);export{y as __pageData,F as default};
